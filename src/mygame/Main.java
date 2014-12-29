5fe70e131d8ddb34a013229be152a6187cba5cd6
Militer <gabi11a@yahoo.com>
2014-12-29 13:50:10 +0200

    Initial Setup

---
+++ src/mygame/Main.java
@@ -0,0 +1,40 @@
+package mygame;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.shape.Box;
+
+/**
+ * test
+ * @author normenhansen
+ */
+public class Main extends SimpleApplication {
+
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        Spatial model = assetManager.loadModel("Models/Spider/blender2ogre-export.j3o");
+
+        rootNode.attachChild(model);
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        //TODO: add update code
+    }
+
+    @Override
+    public void simpleRender(RenderManager rm) {
+        viewPort.setBackgroundColor(ColorRGBA.Blue);
+        //TODO: add render code
+    }
+}
